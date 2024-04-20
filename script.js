150583b212aab50a14858e0ed7a3458fe683e630
MUKUL_reign <99860893+MUKUL2003@users.noreply.github.com>
2024-04-20 12:37:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timer = 60 ;
 var score = 0 ;
 var hitNum ;
-
+//Event Listener on the root of DOM tree , implementing th concept of Event Bubbling
 document.querySelector('.pbtm').addEventListener('click' , function(details){
     var clickedNum = Number(details.target.textContent) ;
     if(hitNum === clickedNum){
@@ -10,17 +10,17 @@ document.querySelector('.pbtm').addEventListener('click' , function(details){
         getNewHit();
     }
 })
-
+//Function to increase the score on hitting correct value
 function increaseScore(){
     score +=10 ;
     document.querySelector('#scoreval').textContent = score ;
 }
-
+//Function to get new hit target 
 function getNewHit(){
     hitNum = Math.floor(Math.random()*10);
     document.querySelector('#hitval').textContent = hitNum ;
 }
-
+//Function that runs the timer 
 function runTimer(){
     var timerint = setInterval(function(){
         if(timer>0){
@@ -33,7 +33,7 @@ function runTimer(){
         }
     } , 1000);
 }
-
+//func to add bubbles with random value in them
 function makeBubble(){
     var clutter="" ;
 
